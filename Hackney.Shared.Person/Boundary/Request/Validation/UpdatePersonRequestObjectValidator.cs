f002ce7da171e4475ebffe3d8494d21c3d68634f
AtiyyahRawat <atiyyah.rawat@gmail.com>
2021-09-27 11:17:19 +0100

    Update Hackney.Shared.Tenure package version

--- Hackney.Shared.Person/Boundary/Request/Validation/UpdatePersonRequestObjectValidator.cs
+++ Hackney.Shared.Person/Boundary/Request/Validation/UpdatePersonRequestObjectValidator.cs
@@ -1,5 +1,6 @@
 using FluentValidation;
 using Hackney.Core.Validation;
+using Hackney.Shared.Tenure.Boundary.Requests.Validation;
 using System;
 
 namespace Hackney.Shared.Person.Boundary.Request.Validation
@@ -49,8 +50,7 @@ namespace Hackney.Shared.Person.Boundary.Request.Validation
                 .NotXssString()
                 .WithErrorCode(ErrorCodes.XssCheckFailure)
                 .When(y => !string.IsNullOrEmpty(y.PlaceOfBirth));
-
-            // RuleForEach(x => x.Tenures).SetValidator(new TenureValidator());
+            RuleForEach(x => x.Tenures).SetValidator(new TenureInformationValidator());
         }
     }
 }
